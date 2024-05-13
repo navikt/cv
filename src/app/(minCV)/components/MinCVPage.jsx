6fea36502109a90d4c0816a7b759d0f37290adaf
BAH <bjorn.arne.hagen@nav.no>
2024-05-13 11:46:56 +0200

    Legg til Stepper

--- src/app/(minCV)/components/MinCVPage.jsx
+++ src/app/(minCV)/components/MinCVPage.jsx
@@ -1,12 +1,59 @@
+"use client";
+
 import styles from "../../page.module.css";
-import { Box, Heading } from "@navikt/ds-react";
+import "@navikt/ds-css";
+import { Box, Heading, Stepper, HStack, VStack, BodyLong, Hide, BodyShort, Show, Detail } from "@navikt/ds-react";
+import { useState } from "react";
 
 export default function MinCVPage() {
+    const [activeStep, setActiveStep] = useState(1);
+
+    function onStepChange(x){
+        setActiveStep(x);
+    }
+
     return (
-        <Box className={styles.main}>
-            <Heading level="1" size="xlarge" align="center" className="mb-12">
-                Min CV
+        <>
+            <Heading level="1" size="xlarge" align="center" className={styles.mb12}>
+                Header
             </Heading>
-        </Box>
+
+            <HStack style={{ display: "flex", alignItems: "flex-start", justifyContent: "center" }}>
+                <div className={styles.sidebar2}>
+                    <Stepper
+                        aria-labelledby="stepper-heading"
+                        orientation="vertical"
+                        className={styles.hideStepper}
+                        activeStep={activeStep}
+                        onStepChange={(x) => onStepChange(x)}
+                    >
+                        <Stepper.Step as="button">Deling av CV</Stepper.Step>
+                        <Stepper.Step as="button">Personalia</Stepper.Step>
+                        <Stepper.Step as="button">Jobbønsker</Stepper.Step>
+                        <Stepper.Step as="button">Utdanning</Stepper.Step>
+                        <Stepper.Step as="button">Fagbrev</Stepper.Step>
+                        <Stepper.Step as="button">Arbeidsforhold</Stepper.Step>
+                        <Stepper.Step as="button">Andre erfaringer</Stepper.Step>
+                        <Stepper.Step as="button">Kompetanser</Stepper.Step>
+                        <Stepper.Step as="button">Offentlige godkjenninger</Stepper.Step>
+                        <Stepper.Step as="button">Andre godkjenninger</Stepper.Step>
+                        <Stepper.Step as="button">Språk</Stepper.Step>
+                        <Stepper.Step as="button">Førerkort</Stepper.Step>
+                        <Stepper.Step as="button">Sammendrag</Stepper.Step>
+                    </Stepper>
+                </div>
+                <div>
+                    <Box className={styles.main}>
+                        <HStack gap="4">
+                            <VStack>
+                            </VStack>
+                        </HStack>
+                    </Box>
+                </div>
+            </HStack>
+            <Heading level="1" size="xlarge" align="center" className={styles.mb12}>
+                Footer
+            </Heading>
+        </>
     );
 }
