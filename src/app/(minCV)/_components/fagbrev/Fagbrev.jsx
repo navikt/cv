3adfa4c1fa7b5eed800e5336f6fd18a23e76173d
BAH <bjorn.arne.hagen@nav.no>
2024-05-24 16:14:36 +0200

    Justeringer Fagbrev

--- src/app/(minCV)/_components/fagbrev/Fagbrev.jsx
+++ src/app/(minCV)/_components/fagbrev/Fagbrev.jsx
@@ -1,23 +1,66 @@
-import { BodyLong, Box, Button, Heading } from "@navikt/ds-react";
+import { BodyLong, Box, Button, Heading, HStack } from "@navikt/ds-react";
 import styles from "@/app/page.module.css";
-import { PlusIcon } from "@navikt/aksel-icons";
+import { PencilIcon, PlusIcon, TrashIcon } from "@navikt/aksel-icons";
+import { useState } from "react";
 
 export default function Fagbrev() {
+    const [fagbrev, setFagbrev] = useState(true);
+
     return (
         <div>
             <Box id="5" background="surface-default" padding="10" className={styles.box}>
-                <Heading level="2" size="large" align="start" spacing>
+                <Heading className={styles.mb6} level="2" size="large" align="start" spacing>
                     Fagbrev
                 </Heading>
-                <BodyLong weight="semibold" spacing>
-                    Du har ikke lagt til noen fagbrev i CV-en
-                </BodyLong>
-                <BodyLong className={styles.mb12}>
-                    Her kan du sette inn ulike fagbrev som du har tatt, f.eks i bilpleie.
-                </BodyLong>
-                <Button icon={<PlusIcon aria-hidden />} variant="primary">
-                    Legg til
-                </Button>
+                {!fagbrev && (
+                    <>
+                        <BodyLong weight="semibold" spacing>
+                            Du har ikke lagt til noen fagbrev i CV-en
+                        </BodyLong>
+                        <BodyLong className={styles.mb12}>
+                            Her kan du sette inn ulike fagbrev som du har tatt, f.eks i bilpleie.
+                        </BodyLong>
+                        <Button icon={<PlusIcon aria-hidden />} variant="primary">
+                            Legg til
+                        </Button>
+                    </>
+                )}
+                {fagbrev && (
+                    <>
+                        <BodyLong weight="semibold">Yrkeskompetanse mediedesigner</BodyLong>
+                        <HStack justify="space-between" className={styles.mb3}>
+                            <Button icon={<PencilIcon aria-hidden />} variant="tertiary">
+                                Endre
+                            </Button>
+                            <Button icon={<TrashIcon aria-hidden />} variant="tertiary">
+                                Fjern
+                            </Button>
+                        </HStack>
+                        <div className={styles.divider}></div>
+                        <BodyLong weight="semibold">Svennebrev profileringsdesigner</BodyLong>
+                        <HStack justify="space-between" className={styles.mb3}>
+                            <Button icon={<PencilIcon aria-hidden />} variant="tertiary">
+                                Endre
+                            </Button>
+                            <Button icon={<TrashIcon aria-hidden />} variant="tertiary">
+                                Fjern
+                            </Button>
+                        </HStack>
+                        <div className={styles.divider}></div>
+                        <BodyLong weight="semibold">Mesterbrev profileringsdesigner</BodyLong>
+                        <HStack justify="space-between" className={styles.mb12}>
+                            <Button icon={<PencilIcon aria-hidden />} variant="tertiary">
+                                Endre
+                            </Button>
+                            <Button icon={<TrashIcon aria-hidden />} variant="tertiary">
+                                Fjern
+                            </Button>
+                        </HStack>
+                        <Button icon={<PlusIcon aria-hidden />} variant="primary">
+                            Legg til flere
+                        </Button>
+                    </>
+                )}
             </Box>
         </div>
     );
