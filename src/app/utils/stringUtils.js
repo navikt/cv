1ed478a9af990738ea136891add4dce3842885de
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-09-23 14:21:15 +0200

    Kjører prettier

--- src/app/utils/stringUtils.js
+++ src/app/utils/stringUtils.js
@@ -1,16 +1,16 @@
-import {MånedEnum} from "@/app/enums/cvEnums";
+import { MånedEnum } from "@/app/enums/cvEnums";
 
 export const formatterTelefon = (telefonnummer) => {
     if (telefonnummer.length === 8 || telefonnummer.length === 11) {
-        const harLandskode = telefonnummer.length === 11 && telefonnummer.includes("+")
-        const landskode = harLandskode ? `${telefonnummer.substring(0, 3)} ` : ""
-        const nummer = harLandskode ? telefonnummer.substring(3) : telefonnummer
+        const harLandskode = telefonnummer.length === 11 && telefonnummer.includes("+");
+        const landskode = harLandskode ? `${telefonnummer.substring(0, 3)} ` : "";
+        const nummer = harLandskode ? telefonnummer.substring(3) : telefonnummer;
 
-        return `${landskode}${nummer.substring(0, 3)} ${nummer.substring(3, 5)} ${nummer.substring(nummer.length - 3)}`
+        return `${landskode}${nummer.substring(0, 3)} ${nummer.substring(3, 5)} ${nummer.substring(nummer.length - 3)}`;
     }
 
-    return telefonnummer
-}
+    return telefonnummer;
+};
 
 export const formatterAdresse = (adresse, postnummer, sted) => {
     if (adresse && postnummer && sted) return `${adresse}, ${postnummer} ${sted}`;
@@ -18,7 +18,7 @@ export const formatterAdresse = (adresse, postnummer, sted) => {
     else if (adresse && sted) return `${adresse}, ${sted}`;
     else if (postnummer && sted) return `${postnummer} ${sted}`;
     else return adresse || postnummer || sted || "";
-}
+};
 
 // Her trenger vi design for hvordan vi skal reprsentere valgte lokasjoner / yrker i modalen
 export const formatterListeAvObjekterTilTekst = (liste, felt) => {
@@ -32,5 +32,5 @@ export const formatterListeAvObjekterTilTekst = (liste, felt) => {
 export const formatterDato = (dato) => {
     if (!dato) return "nå";
     const date = new Date(dato);
-    return `${MånedEnum[date.getMonth()]} ${date.getFullYear()}`
-}
+    return `${MånedEnum[date.getMonth()]} ${date.getFullYear()}`;
+};
