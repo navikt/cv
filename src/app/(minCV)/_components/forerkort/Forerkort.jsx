1d8944423264a48735cc750ffe6aa4d3ec86fc49
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-09-27 14:54:20 +0200

    Legger inn dataflyt i førerkort og lager modal for den

--- src/app/(minCV)/_components/forerkort/Forerkort.jsx
+++ src/app/(minCV)/_components/forerkort/Forerkort.jsx
@@ -1,22 +1,102 @@
-import { BodyLong, Box, Button, Heading } from "@navikt/ds-react";
+import { BodyLong, Box, Button, Heading, HStack, VStack } from "@navikt/ds-react";
 import styles from "@/app/page.module.css";
-import { PlusIcon } from "@navikt/aksel-icons";
+import { PencilIcon, PlusIcon, TrashIcon } from "@navikt/aksel-icons";
+import { useContext, useEffect, useState } from "react";
+import { CvOgPersonContext } from "@/app/(minCV)/_components/context/CvContext";
+import { formatterDato } from "@/app/utils/stringUtils";
+import FørerkortModal from "@/app/(minCV)/_components/forerkort/FørerkortModal";
 
 export default function Forerkort() {
+    const cvContext = useContext(CvOgPersonContext).cv;
+    const [førerkort, setFørerkort] = useState([]);
+    const [modalÅpen, setModalÅpen] = useState(false);
+    const [gjeldendeFørerkort, setGjeldendeFørerkort] = useState(-1);
+
+    useEffect(() => {
+        const oppdaterFørerkort = (førerkort) => setFørerkort(førerkort);
+        if (cvContext.status === "success") oppdaterFørerkort(cvContext.data.foererkort || []);
+    }, [cvContext]);
+
+    const toggleModal = (åpen, index) => {
+        setGjeldendeFørerkort(index >= 0 ? index : -1);
+        setModalÅpen(åpen);
+    };
+
+    const lagreFørerkort = (oppdatertFørerkort) => {
+        const oppdaterteFørerkort = [...førerkort];
+        if (gjeldendeFørerkort >= 0) oppdaterteFørerkort.splice(gjeldendeFørerkort, 1, oppdatertFørerkort);
+        else oppdaterteFørerkort.push(oppdatertFørerkort);
+
+        // TODO: Send oppdatering til backend og oppdater data med responsen / feilmelding
+        setFørerkort(oppdaterteFørerkort);
+        setModalÅpen(false);
+    };
+
+    const slettFørerkort = (index) => {
+        const oppdaterteFørerkort = [...førerkort];
+        oppdaterteFørerkort.splice(index, 1);
+        setFørerkort(oppdaterteFørerkort);
+    };
     return (
         <div id="12">
             <Box background="surface-default" padding="10" className={styles.box}>
                 <Heading level="2" size="large" align="start" spacing>
                     Førerkort
                 </Heading>
-                <BodyLong weight="semibold" spacing>
-                    Du har ikke lagt til noen førerkort i CV-en
-                </BodyLong>
-                <BodyLong className={styles.mb12}>Her kan du sette inn alle ulike førerkort, f.eks lastebil.</BodyLong>
-                <Button icon={<PlusIcon aria-hidden />} variant="primary">
+                {førerkort.length === 0 ? (
+                    <div>
+                        <BodyLong weight="semibold" spacing>
+                            Du har ikke lagt til noen førerkort i CV-en
+                        </BodyLong>
+                        <BodyLong className={styles.mb12}>
+                            Her kan du sette inn alle ulike førerkort, f.eks lastebil.
+                        </BodyLong>
+                    </div>
+                ) : (
+                    <div className={styles.mb6}>
+                        {førerkort.map((fk, index) => (
+                            <div key={index}>
+                                <VStack className={styles.mb3}>
+                                    <BodyLong weight="semibold">• {fk.type}</BodyLong>
+                                    {fk.acquiredDate && (
+                                        <BodyLong weight="regular">
+                                            {`Gyldig fra ${formatterDato(fk.acquiredDate)} - ${formatterDato(fk.expiryDate)}`}
+                                        </BodyLong>
+                                    )}
+                                </VStack>
+                                <HStack justify="space-between" className={styles.mb3}>
+                                    <Button
+                                        icon={<PencilIcon aria-hidden />}
+                                        variant="tertiary"
+                                        onClick={() => toggleModal(true, index)}
+                                    >
+                                        Endre
+                                    </Button>
+                                    <Button
+                                        icon={<TrashIcon aria-hidden />}
+                                        variant="tertiary"
+                                        onClick={() => slettFørerkort(index)}
+                                    >
+                                        Fjern
+                                    </Button>
+                                </HStack>
+                                {index < førerkort.length - 1 && <div className={styles.divider}></div>}
+                            </div>
+                        ))}
+                    </div>
+                )}
+                <Button icon={<PlusIcon aria-hidden />} variant="primary" onClick={() => toggleModal(true)}>
                     Legg til
                 </Button>
             </Box>
+            {modalÅpen && (
+                <FørerkortModal
+                    modalÅpen={modalÅpen}
+                    toggleModal={toggleModal}
+                    førerkort={førerkort[gjeldendeFørerkort]}
+                    lagreFørerkort={lagreFørerkort}
+                />
+            )}
         </div>
     );
 }
---
+++ src/app/(minCV)/_components/forerkort/FørerkortModal.jsx
@@ -0,0 +1,86 @@
+import {Button, Heading, HStack, Modal, Select, VStack} from "@navikt/ds-react";
+import { useEffect, useState } from "react";
+import fagbrevMock from "../../../mocks/typeahead/fagbrevTypeaheadMock.json";
+import { Typeahead } from "@/app/(minCV)/_components/typeahead/Typeahead";
+import styles from "@/app/page.module.css";
+import { SpråkEnum } from "@/app/enums/cvEnums";
+import { gyldigeFørerkort } from "@/app/data/førerkort.json";
+import {Datovelger} from "@/app/(minCV)/_components/datovelger/Datovelger";
+
+export default function FørerkortModal({ modalÅpen, toggleModal, førerkort, lagreFørerkort }) {
+    const [valgtFørerkort, setValgtFørerkort] = useState(førerkort || null);
+    const [gyldigFra, setGyldigFra] = useState(førerkort?.acquiredDate ? new Date(førerkort.acquiredDate) : null);
+    const [gyldigTil, setGyldigTil] = useState(førerkort?.expiryDate ? new Date(førerkort.expiryDate) : null);
+    const [kreverDato, setKreverDato] = useState(!!førerkort?.acquiredDate);
+
+    useEffect(() => {
+        const oppdaterFørerkort = (førerkort) => setValgtFørerkort(førerkort);
+        oppdaterFørerkort(førerkort || []);
+    }, [førerkort]);
+
+    const velgFørerkort = (verdi) => {
+        const valgtFørerkort = gyldigeFørerkort.find((e) => e.type === verdi);
+        setValgtFørerkort(valgtFørerkort);
+        setKreverDato(valgtFørerkort?.kreverDato || false);
+    };
+
+    const lagre = () => {
+        lagreFørerkort({
+            type: valgtFørerkort.label || valgtFørerkort.type,
+            acquiredDate: gyldigFra,
+            expiryDate: gyldigTil,
+        });
+        setValgtFørerkort(null);
+    };
+
+    return (
+        <Modal
+            open={modalÅpen}
+            aria-label="Legg til fagbrev"
+            onClose={() => toggleModal(false)}
+            width="medium"
+            className={"overflow-visible"}
+        >
+            <Modal.Header closeButton={true}>
+                <Heading align="start" level="3" size="medium">
+                    <HStack gap="1" align="center">
+                        Legg til Førerkort
+                    </HStack>
+                </Heading>
+            </Modal.Header>
+            <Modal.Body style={{ padding: "1rem 2.8rem 2.5rem 2.8rem" }} className={"overflow-visible"}>
+                <VStack>
+                    <Select
+                        id="Førerkort"
+                        label="Førerkort"
+                        className={styles.mb6}
+                        value={valgtFørerkort?.type || ""}
+                        onChange={(e) => velgFørerkort(e.target.value)}
+                    >
+                        {gyldigeFørerkort.map((e) => (
+                            <option key={e.type} value={e.type}>
+                                {e.type}
+                            </option>
+                        ))}
+                    </Select>
+                    {kreverDato && (
+                        <HStack gap="8">
+                            <Datovelger valgtDato={gyldigFra} oppdaterDato={setGyldigFra} label="Gyldig fra" obligatorisk />
+                            <Datovelger valgtDato={gyldigTil} oppdaterDato={setGyldigTil} label="Gyldig til" obligatorisk fremtid />
+                        </HStack>
+                    )}
+                </VStack>
+            </Modal.Body>
+            <Modal.Footer>
+                <HStack gap="4">
+                    <Button variant="secondary" onClick={() => toggleModal(false)}>
+                        Avbryt
+                    </Button>
+                    <Button variant="primary" onClick={() => lagre(valgtFørerkort)}>
+                        Lagre
+                    </Button>
+                </HStack>
+            </Modal.Footer>
+        </Modal>
+    );
+}
