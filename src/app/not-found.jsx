cf74ef4a2867cab9b170382baec3e5a8f169a09e
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-11-01 09:02:11 +0100

    Fikser opp not-found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,21 @@
-import { BodyLong, Box, Button, Heading, HStack, VStack } from "@navikt/ds-react";
+import { BodyLong, Box, Heading, HStack, Link, VStack } from "@navikt/ds-react";
 import styles from "@/app/page.module.css";
-import { arbeidsplassenBaseUrl } from "@/app/_common/utils/urlUtils";
+import { arbeidsplassenBaseUrl, navBaseUrl } from "@/app/_common/utils/urlUtils";
+import { Pictogram } from "@/app/_common/components/HeaderPanel";
 
 export default function NotFound() {
     return (
-        <HStack className={styles.pageContainer}>
-            <Box padding="10" className={styles.mt3}>
-                <VStack>
+        <VStack>
+            <Box as="header" className={styles.notFoundHeader}>
+                <HStack className={styles.notFoundHeaderContainer}>
+                    <Pictogram />
+                    <Heading level="1" size="large">
+                        Din CV
+                    </Heading>
+                </HStack>
+            </Box>
+            <HStack className={styles.pageContainer}>
+                <VStack className={styles.mt3}>
                     <Heading level="1" size="xlarge" className="text-center" spacing>
                         Beklager, vi fant ikke siden
                     </Heading>
@@ -14,13 +23,22 @@ export default function NotFound() {
                         Denne siden kan være slettet eller flyttet, eller det er en feil i lenken.
                     </BodyLong>
 
-                    <HStack gap="4" className={styles.mb3}>
-                        <Button variant="primary" as="a" href={`${arbeidsplassenBaseUrl}/personbruker`}>
-                            Gå til CV
-                        </Button>
+                    <HStack className={styles.notFoundHStack}>
+                        <Link
+                            href={`${arbeidsplassenBaseUrl}/personbruker`}
+                            className={[styles.notFoundButton, styles.notFoundButtonPrimary]}
+                        >
+                            Tilbake til CV
+                        </Link>
+                        <Link
+                            href={`${navBaseUrl}/minside`}
+                            className={[styles.notFoundButton, styles.notFoundButtonTertiary]}
+                        >
+                            Gå til Min Side
+                        </Link>
                     </HStack>
                 </VStack>
-            </Box>
-        </HStack>
+            </HStack>
+        </VStack>
     );
 }
