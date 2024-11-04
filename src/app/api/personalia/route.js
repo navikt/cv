cc77a5bf29484b1a963e2c4d098bafd54149fd7e
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-11-04 11:38:53 +0100

    WIP observability 2

--- src/app/api/personalia/route.js
+++ src/app/api/personalia/route.js
@@ -1,10 +1,10 @@
 import { serverConfig } from "@/app/_common/serverConfig";
 import { putData } from "@/app/api/utils";
-import AppMetrics from "@/app/_common/observability/prometheus";
+import metrics from "@/app/_common/observability/prometheus";
 
 export async function PUT(request) {
     const cvApiBaseUrl = serverConfig?.urls?.cvApi;
     const fullUrl = `${cvApiBaseUrl}/v2/personalia`;
-    new AppMetrics().cvOppdateringCounter.inc("personalia", 1);
+    metrics.cvOppdateringCounter.inc("personalia", 1);
     return putData(fullUrl, serverConfig?.audience?.cvApi, request);
 }
