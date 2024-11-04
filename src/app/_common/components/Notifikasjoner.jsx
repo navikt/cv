34b3a72c47b286c33d8ede9fcfbb9d4d5176af6a
Audun Skjelvan <audun.skjelvan@nav.no>
2024-11-04 14:12:49 +0100

    adjust settings

--- src/app/_common/components/Notifikasjoner.jsx
+++ src/app/_common/components/Notifikasjoner.jsx
@@ -3,11 +3,11 @@ import styles from "../../page.module.css";
 
 export function Notifikasjoner({ notifikasjoner }) {
     return (
-        <VStack aria-atomic="true" gap="4" className={styles.notifikasjonContainer}>
+        <VStack aria-atomic="false" gap="4" className={styles.notifikasjonContainer}>
             {notifikasjoner.map((notifikasjon) => (
                 <Alert
                     aria-live="polite"
-                    role="status"
+                    role="alert"
                     className={styles.notifikasjon}
                     id={notifikasjon.id}
                     key={notifikasjon.id}
