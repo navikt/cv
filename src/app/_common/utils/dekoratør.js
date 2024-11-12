a1497907e58118d0766b32263f013e0a36409aad
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2024-11-12 12:00:25 +0100

    Sjekker innlogging hvert minutt og redirecter til minside ved utlogging (#79)
    * Sjekker innlogging hvert minutt og redirecter til minside ved utlogging
    
    * Redirecter til app med ?logged_out=true for å informere om utlogging

--- src/app/_common/utils/dekoratør.js
+++ src/app/_common/utils/dekoratør.js
@@ -7,6 +7,7 @@ const dekoratørProps = {
         utilsBackground: "white",
         context: "privatperson",
         redirectToApp: true,
+        redirectToUrlLogout: "/personbruker?logged_out=true",
         breadcrumbs: [
             {
                 title: "Min side",
