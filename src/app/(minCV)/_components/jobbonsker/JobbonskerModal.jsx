e62f8b239fe6f84634f9d2360fd28b47f43bbc4c
Audun Skjelvan <audun.skjelvan@nav.no>
2024-11-14 16:30:20 +0100

    initial commit validate jobbonsker

--- src/app/(minCV)/_components/jobbonsker/JobbonskerModal.jsx
+++ src/app/(minCV)/_components/jobbonsker/JobbonskerModal.jsx
@@ -5,8 +5,13 @@ import { useEffect, useState } from "react";
 import { Typeahead } from "@/app/(minCV)/_components/typeahead/Typeahead";
 import { TypeaheadEnum } from "@/app/_common/enums/typeaheadEnums";
 import { CvModalForm } from "@/app/_common/components/CvModalForm";
+import { ValidationErrors } from "@/app/_common/components/ValidationErrors";
+import { handleZodValidation, revalidate } from "@/app/_common/utils/validationHelper";
+import z from "zod";
 
 export function JobbonskerModal({ modalÅpen, toggleModal, gjeldendeElement, lagreElement, laster, feilet }) {
+    const [shouldAutoFocusErrors, setShouldAutoFocusErrors] = useState(false);
+    const [errors, setErrors] = useState({});
     const [yrker, setYrker] = useState([]);
     const [lokasjoner, setLokasjoner] = useState([]);
     const [omfang, setOmfang] = useState([]);
@@ -20,30 +25,91 @@ export function JobbonskerModal({ modalÅpen, toggleModal, gjeldendeElement, lag
         const oppdaterJobbønsker = (jobbønsker) => {
             setYrker(jobbønsker?.occupations || []);
             setLokasjoner(jobbønsker?.locations || []);
-            setOmfang(jobbønsker?.workLoadTypes || []);
-            setAnsettelsesform(jobbønsker?.occupationTypes || []);
-            setArbeidstid(jobbønsker?.workScheduleTypes || []);
-            setStarttidspunkt(jobbønsker?.startOption || "ETTER_TRE_MND");
+            // setOmfang(jobbønsker?.workLoadTypes || []);
+            // setAnsettelsesform(jobbønsker?.occupationTypes || []);
+            // setArbeidstid(jobbønsker?.workScheduleTypes || []);
+            // setStarttidspunkt(jobbønsker?.startOption || "ETTER_TRE_MND");
         };
 
         oppdaterJobbønsker(gjeldendeElement);
     }, [gjeldendeElement]);
 
-    const lagre = () => {
-        if (yrker.length === 0) setYrkerError(true);
-        if (lokasjoner.length === 0) setLokasjonerError(true);
+    const JobbonskerSchema = z.object({
+        occupations: z.string().min(1, "Du må legge til jobbønsker"),
+        locations: z.string().min(1, "Du må legge til steder"),
+        workLoadTypes: z.string().optional(),
+        occupationTypes: z.string().optional(),
+        workScheduleTypes: z.string().optional(),
+        startOption: z.string().optional(),
+    });
 
-        if (yrker.length !== 0 && lokasjoner.length !== 0) {
-            lagreElement({
-                ...gjeldendeElement,
-                occupations: yrker,
-                locations: lokasjoner,
-                workLoadTypes: omfang,
-                occupationTypes: ansettelsesform,
-                workScheduleTypes: arbeidstid,
-                startOption: starttidspunkt,
-            });
-        }
+    // const lagre = (e) => {
+    //     const data = Object.fromEntries(new FormData(e.currentTarget));
+    //     setShouldAutoFocusErrors(true);
+
+    //     handleZodValidation({
+    //         onError: setErrors,
+    //         data: data,
+    //         onSuccess: (res) => {
+    //             lagrePersonalia({
+    //                 fornavn: res.fornavn,
+    //                 etternavn: res.etternavn,
+    //                 epost: res.epost,
+    //                 telefonnummer: res.telefonnummer,
+    //                 adresse: res.adresse,
+    //                 postnummer: res.postnummer,
+    //                 poststed: res.poststed,
+    //             });
+    //         },
+    //         schema: PersonaliaSchema,
+    //     });
+    // };
+
+    const lagre = (e) => {
+        const formData = new FormData(e.currentTarget);
+
+        formData.append("occupations", JSON.stringify(yrker));
+        formData.append("locations", JSON.stringify(lokasjoner));
+        const data = Object.fromEntries(
+            Array.from(formData.keys()).map((key) => [
+                key,
+                formData.getAll(key).length > 1 ? formData.getAll(key) : formData.get(key),
+            ]),
+        );
+
+        setShouldAutoFocusErrors(true);
+
+        handleZodValidation({
+            onError: setErrors,
+            data: data,
+            onSuccess: (res) => {
+                lagreElement({
+                    ...gjeldendeElement,
+                    occupations: yrker,
+                    locations: lokasjoner,
+                    workLoadTypes: omfang,
+                    occupationTypes: ansettelsesform,
+                    workScheduleTypes: arbeidstid,
+                    startOption: starttidspunkt,
+                });
+            },
+            schema: JobbonskerSchema,
+        });
+
+        // if (yrker.length === 0) setYrkerError(true);
+        // if (lokasjoner.length === 0) setLokasjonerError(true);
+
+        // if (yrker.length !== 0 && lokasjoner.length !== 0) {
+        //     lagreElement({
+        //         ...gjeldendeElement,
+        //         occupations: yrker,
+        //         locations: lokasjoner,
+        //         workLoadTypes: omfang,
+        //         occupationTypes: ansettelsesform,
+        //         workScheduleTypes: arbeidstid,
+        //         startOption: starttidspunkt,
+        //     });
+        // }
     };
 
     const oppdaterYrker = (yrke, erValgt) => {
@@ -108,48 +174,53 @@ export function JobbonskerModal({ modalÅpen, toggleModal, gjeldendeElement, lag
                 />
             </VStack>
             <CheckboxGroup
+                id="workLoadTypes"
                 className={styles.mb6}
-                onChange={setOmfang}
-                value={omfang}
+                defaultValue={gjeldendeElement.workLoadTypes}
                 legend="Vil du jobbe heltid eller deltid?"
             >
                 <HStack gap="4">
                     {Object.keys(OmfangEnum).map((key) => (
-                        <Checkbox value={key} key={key}>
+                        <Checkbox name="workLoadTypes" value={key} key={key}>
                             {OmfangEnum[key]}
                         </Checkbox>
                     ))}
                 </HStack>
             </CheckboxGroup>
             <CheckboxGroup
+                id="workScheduleTypes"
                 className={styles.mb6}
-                onChange={setArbeidstid}
-                value={arbeidstid}
+                defaultValue={gjeldendeElement.workScheduleTypes}
                 legend="Når kan du jobbe?"
             >
                 <HStack>
                     {Object.keys(ArbeidstidEnum).map((verdi) => (
-                        <Checkbox className={styles.checkbox} key={verdi} value={verdi}>
+                        <Checkbox name="workScheduleTypes" className={styles.checkbox} key={verdi} value={verdi}>
                             {ArbeidstidEnum[verdi]}
                         </Checkbox>
                     ))}
                 </HStack>
             </CheckboxGroup>
             <CheckboxGroup
+                id="occupationTypes"
                 className={styles.mb6}
-                onChange={setAnsettelsesform}
-                value={ansettelsesform}
+                defaultValue={gjeldendeElement.occupationTypes}
                 legend="Hva slags ansettelse ønsker du?"
             >
                 <HStack>
                     {Object.keys(AnsettelsesformEnum).map((verdi) => (
-                        <Checkbox className={styles.checkbox} key={verdi} value={verdi}>
+                        <Checkbox name="occupationTypes" className={styles.checkbox} key={verdi} value={verdi}>
                             {AnsettelsesformEnum[verdi]}
                         </Checkbox>
                     ))}
                 </HStack>
             </CheckboxGroup>
-            <RadioGroup legend="Når kan du begynne i ny jobb?" onChange={setStarttidspunkt} value={starttidspunkt}>
+            <RadioGroup
+                id="startOption"
+                name="startOption"
+                legend="Når kan du begynne i ny jobb?"
+                defaultValue={gjeldendeElement.startOption}
+            >
                 <HStack>
                     {Object.keys(StarttidspunktEnum).map((verdi) => (
                         <Radio className={styles.checkbox} key={verdi} value={verdi}>
@@ -158,6 +229,7 @@ export function JobbonskerModal({ modalÅpen, toggleModal, gjeldendeElement, lag
                     ))}
                 </HStack>
             </RadioGroup>
+            <ValidationErrors shouldAutoFocusErrors={shouldAutoFocusErrors} validationErrors={errors} />
         </CvModalForm>
     );
 }
