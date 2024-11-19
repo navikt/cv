106914e535d54d9440d25ccad2cf859a2f110532
Audun Skjelvan <audun.skjelvan@nav.no>
2024-11-19 13:22:39 +0100

    add error summary to språk form

--- src/app/(minCV)/_components/sprak/SpråkModal.jsx
+++ src/app/(minCV)/_components/sprak/SpråkModal.jsx
@@ -5,12 +5,13 @@ import { SpråkEnum } from "@/app/_common/enums/cvEnums";
 import styles from "@/app/page.module.css";
 import { TypeaheadEnum } from "@/app/_common/enums/typeaheadEnums";
 import { CvModalForm } from "@/app/_common/components/CvModalForm";
+import { ValidationErrors } from "@/app/_common/components/ValidationErrors";
 
 export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement, lagreElement, laster, feilet }) {
+    const [errors, setErrors] = useState({});
     const [valgtSpråk, setValgtSpråk] = useState(gjeldendeElement || null);
     const [muntligEvne, setMuntligEvne] = useState("IKKE_OPPGITT");
     const [skriftligEvne, setSkriftligEvne] = useState("IKKE_OPPGITT");
-    const [valgtSprakError, setValgtSprakError] = useState(false);
 
     useEffect(() => {
         const oppdaterSpråk = (språk) => {
@@ -22,7 +23,7 @@ export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement,
     }, [gjeldendeElement]);
 
     const lagre = () => {
-        if (!valgtSpråk || valgtSpråk.length === 0) setValgtSprakError(true);
+        if (!valgtSpråk || valgtSpråk.length === 0) setErrors({ language: "Du må velge et språk" });
 
         if (valgtSpråk && valgtSpråk.length !== 0) {
             lagreElement({
@@ -36,7 +37,7 @@ export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement,
 
     const oppdaterValgtSpråk = (verdi, erValgt) => {
         setValgtSpråk(erValgt ? verdi : null);
-        setValgtSprakError(false);
+        setErrors({});
     };
 
     return (
@@ -50,6 +51,8 @@ export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement,
             overflowVisible
         >
             <Typeahead
+                id="language"
+                name="language"
                 className={styles.mb6}
                 label="Språk"
                 description="Må fylles ut"
@@ -57,7 +60,7 @@ export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement,
                 oppdaterValg={oppdaterValgtSpråk}
                 type={TypeaheadEnum.SPRÅK}
                 forhåndshentet
-                error={valgtSprakError && "Du må velge et språk"}
+                error={errors?.language}
             />
             <Select
                 id="Muntlig"
@@ -85,6 +88,7 @@ export default function SpråkModal({ modalÅpen, toggleModal, gjeldendeElement,
                     </option>
                 ))}
             </Select>
+            <ValidationErrors shouldAutoFocusErrors validationErrors={errors} />
         </CvModalForm>
     );
 }
