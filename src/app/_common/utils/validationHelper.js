8c3925bff719708ff71bb74f7e1caad77ea45e88
Audun Skjelvan <audun.skjelvan@nav.no>
2024-11-19 12:22:32 +0100

    console

--- src/app/_common/utils/validationHelper.js
+++ src/app/_common/utils/validationHelper.js
@@ -54,8 +54,6 @@ export const revalidate = (e, schema, errors, setErrors) => {
 };
 
 export const revalidateExplicitValue = (name, value, schema, errors, setErrors) => {
-    console.log("NAME", schema);
-    console.log("VALUE", value);
     const data = { [name]: value };
     const oneValueSchema = z.object({ [name]: schema.shape[name] });
 
