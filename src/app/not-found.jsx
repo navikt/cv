8f55af6a89858ac3e5759eb470a27e4ab69a873d
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-12-03 09:51:52 +0100

    Importer css fra aksel i statiske sider i stedet for å prøve å løse det selv

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,44 +1,41 @@
-import { BodyLong, Box, Heading, HStack, Link, VStack } from "@navikt/ds-react";
+import { BodyLong, Box, Button, Heading, HStack, VStack } from "@navikt/ds-react";
+import "@navikt/ds-css";
 import styles from "@/app/page.module.css";
-import { arbeidsplassenBaseUrl, navBaseUrl } from "@/app/_common/utils/urlUtils";
 import { Pictogram } from "@/app/_common/components/HeaderPanel";
+import NextLink from "next/link";
 
 export default function NotFound() {
     return (
         <VStack>
-            <Box as="header" className={styles.staticHeaderContainer}>
-                <HStack className={styles.staticHeaderContent}>
+            <Box as="header" borderWidth="0 0 4 0" borderColor="surface-info" background="surface-default">
+                <HStack gap="3" justify="center" align="center" className={styles.mb9}>
                     <Pictogram />
                     <Heading level="1" size="large">
                         Min CV
                     </Heading>
                 </HStack>
             </Box>
-            <HStack className={styles.pageContainer}>
-                <VStack className={styles.mt3}>
-                    <Heading level="1" size="xlarge" className="text-center" spacing>
-                        Beklager, vi fant ikke siden
-                    </Heading>
-                    <BodyLong className={["text-center", styles.mb12]}>
-                        Denne siden kan være slettet eller flyttet, eller det er en feil i lenken.
-                    </BodyLong>
+            <VStack align="center" className={styles.mt16}>
+                <Heading level="1" size="large" align="center" className="text-center" spacing>
+                    Beklager, vi fant ikke siden
+                </Heading>
+                <BodyLong align="center" className={styles.mb12}>
+                    Denne siden kan være slettet eller flyttet, eller det er en feil i lenken.
+                </BodyLong>
 
-                    <HStack className={styles.notFoundHStack}>
-                        <Link
-                            href={`${arbeidsplassenBaseUrl}/min-cv`}
-                            className={[styles.notFoundButton, styles.notFoundButtonPrimary]}
-                        >
+                <HStack gap="4" className={styles.mb16}>
+                    <NextLink href="https://www.nav.no/min-cv" passHref legacyBehavior>
+                        <Button variant="primary" as="a" role="link">
                             Tilbake til CV
-                        </Link>
-                        <Link
-                            href={`${navBaseUrl}/minside`}
-                            className={[styles.notFoundButton, styles.notFoundButtonTertiary]}
-                        >
+                        </Button>
+                    </NextLink>
+                    <NextLink href="https://www.nav.no/minside" passHref legacyBehavior>
+                        <Button variant="secondary" as="a" role="link">
                             Gå til Min Side
-                        </Link>
-                    </HStack>
-                </VStack>
-            </HStack>
+                        </Button>
+                    </NextLink>
+                </HStack>
+            </VStack>
         </VStack>
     );
 }
