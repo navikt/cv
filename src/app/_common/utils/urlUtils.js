205dac1f839d5aa985ccf218bdd497e126a94a26
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-12-09 10:54:23 +0100

    Bugfix på navBaseUrl

--- src/app/_common/utils/urlUtils.js
+++ src/app/_common/utils/urlUtils.js
@@ -1,5 +1,5 @@
 export const navBaseUrl =
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "https://www.nav.no" : "https://www.ansatt.dev.nav.no";
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "https://www.ansatt.dev.nav.no" : "https://www.nav.no";
 
 export const arbeidsplassenBaseUrl =
     process.env.NEXT_PUBLIC_ENVIRONMENT === "dev"
