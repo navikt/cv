078ed194c9e6d61b963dd5e9647ac71eab86133b
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2024-12-09 10:47:09 +0100

    Snur bruken av NEXT_PUBLIC_ENVIRONMENT for å defaulte til prod dersom den ikke eksisterer (#149)

--- src/app/_common/utils/urlUtils.js
+++ src/app/_common/utils/urlUtils.js
@@ -1,7 +1,7 @@
 export const navBaseUrl =
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "prod" ? "https://www.nav.no" : "https://www.ansatt.dev.nav.no";
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "dev" ? "https://www.nav.no" : "https://www.ansatt.dev.nav.no";
 
 export const arbeidsplassenBaseUrl =
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "prod"
-        ? "https://arbeidsplassen.nav.no"
-        : "https://arbeidsplassen.intern.dev.nav.no";
+    process.env.NEXT_PUBLIC_ENVIRONMENT === "dev"
+        ? "https://arbeidsplassen.intern.dev.nav.no"
+        : "https://arbeidsplassen.nav.no";
