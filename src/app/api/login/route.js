26618a18fd7fff821a0e75aebbd94bbba424c51b
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-12-16 13:47:51 +0100

    Defaulter til prodverdier slik at prod ihvertfall blir riktig

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,8 +1,8 @@
 import { serverConfig } from "@/app/_common/serverConfig";
 
 export async function GET() {
-    const loginUrl = serverConfig?.urls.login;
-    const baseUrl = serverConfig?.urls.base;
+    const loginUrl = serverConfig?.urls.login || "https://login.nav.no/oauth2/login";
+    const baseUrl = serverConfig?.urls.base || "https://www.nav.no/min-cv";
 
     const loginRedirect = `${loginUrl}?redirect=${baseUrl}&level=Level3`;
 
