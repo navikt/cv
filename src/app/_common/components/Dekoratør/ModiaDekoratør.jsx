2b3c7a47fff9bf2477c6d4d3276add481037224d
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-01-14 13:21:28 +0100

    Fjern proxy og gjør dekoratørimport dynamisk

--- src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
+++ src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
@@ -1,17 +1,23 @@
 "use client";
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import NAVSPA from "@navikt/navspa";
 import { getMiljø, Miljø } from "@/app/_common/utils/miljøUtils";
+import dynamic from "next/dynamic";
 
 export default function ModiaDekoratør() {
-    const InternflateDecorator = NAVSPA.importer("internarbeidsflate-decorator-v3");
-
     const miljø = getMiljø() === Miljø.PROD ? "prod" : "q0";
+
+    const InternflateDecorator = dynamic(
+        () => import("@navikt/navspa").then((NAVSPA) => NAVSPA.default.importer("internarbeidsflate-decorator-v3")),
+        {
+            ssr: false,
+            // eslint-disable-next-line react/no-unstable-nested-components
+            loading: () => <div>Laster dekoratør...</div>,
+        },
+    );
 
     return (
         <InternflateDecorator
-            proxy
             appName="cv-veileder"
             environment={miljø}
             showEnheter
