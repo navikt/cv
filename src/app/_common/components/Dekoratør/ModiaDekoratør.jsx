5fca057aca56b18cd963d691cdd51631e366181c
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-01-14 14:27:04 +0100

    Legg inn full proxyUrl

--- src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
+++ src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 
 export default function ModiaDekoratør() {
     const miljø = getMiljø() === Miljø.PROD ? "prod" : "q0";
+    const proxyUrl = `https://cv-veileder.intern${getMiljø() === Miljø.PROD ? "" : ".dev"}.nav.no/min-cv`;
 
     const InternflateDecorator = dynamic(
         () => import("@navikt/navspa").then((NAVSPA) => NAVSPA.default.importer("internarbeidsflate-decorator-v3")),
@@ -16,8 +17,11 @@ export default function ModiaDekoratør() {
         },
     );
 
+    console.log(`Miljø: ${miljø}, proxyUrl: ${proxyUrl}`);
+
     return (
         <InternflateDecorator
+            proxy={proxyUrl}
             appName="cv-veileder"
             environment={miljø}
             showEnheter
