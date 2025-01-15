3795ace02b842f5cb1c6024c56c27ee25509116f
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-01-15 12:56:57 +0100

    Logger i route.js

--- src/app/api/decorator/route.js
+++ src/app/api/decorator/route.js
@@ -1,10 +1,15 @@
 import { serverConfig } from "@/app/_common/serverConfig";
+import { logger } from "@navikt/next-logger";
 import { proxyWithOBO } from "../../_common/utils/oboProxy";
 
 export async function GET(request) {
     const { erVeileder, urls, scope } = serverConfig;
 
     if (!erVeileder) return; // 404 response
+
+    logger.info(
+        `Er i decorator/route.js med auth-header: ${request.headers.get("authorization")}. Alle headers: ${request.headers}`,
+    );
 
     return proxyWithOBO(urls.modiaDekorator, "", scope.modiaDekorator, request);
 }
