01a5134ae0c29281d8c4f8aa27613920d2f3781d
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-01-20 07:53:56 +0100

    Fiks return-verdier fra decorator/route

--- src/app/api/decorator/route.js
+++ src/app/api/decorator/route.js
@@ -1,15 +1,16 @@
 import { serverConfig } from "@/app/_common/serverConfig";
 import { logger } from "@navikt/next-logger";
+import { NextResponse } from "next/server";
 import { fetchWithObo } from "../../_common/utils/tokenUtils/oboProxy";
 
 export async function GET(request) {
     const { erVeileder, urls, scope } = serverConfig;
 
-    if (!erVeileder) return; // 404 response
+    if (!erVeileder) return NextResponse.json({ error: "Not Found" }, { status: 404 });
 
     logger.info(
         `Er i decorator/route.js med auth-header: ${request.headers.get("authorization")}. Alle headers: ${request.headers}`,
     );
 
-    await fetchWithObo(`${urls.modiaDekorator}/decorator/v2`, scope.modiaDekorator, request);
+    return fetchWithObo(`${urls.modiaDekorator}/decorator/v2`, scope.modiaDekorator, request);
 }
