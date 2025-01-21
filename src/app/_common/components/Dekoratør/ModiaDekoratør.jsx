d2e2e29ef284655481e3e1c0a88d3f6f29c47806
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-01-21 12:03:39 +0100

    Prøver med statisk dekoratørimport

--- src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
+++ src/app/_common/components/Dekoratør/ModiaDekoratør.jsx
@@ -2,20 +2,13 @@
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { getMiljø, Miljø } from "@/app/_common/utils/miljøUtils";
-import dynamic from "next/dynamic";
+import * as NAVSPA from "@navikt/navspa";
 
 export default function ModiaDekoratør() {
     const miljø = getMiljø() === Miljø.PROD ? "prod" : "q0";
     const proxyUrl = `https://cv-veileder.intern${getMiljø() === Miljø.PROD ? "" : ".dev"}.nav.no/min-cv/api/veileder`;
 
-    const InternflateDecorator = dynamic(
-        () => import("@navikt/navspa").then((NAVSPA) => NAVSPA.default.importer("internarbeidsflate-decorator-v3")),
-        {
-            ssr: false,
-            // eslint-disable-next-line react/no-unstable-nested-components
-            loading: () => <div>Laster dekoratør...</div>,
-        },
-    );
+    const InternflateDecorator = NAVSPA.importer("internarbeidsflate-decorator-v3");
 
     console.log(`Miljø: ${miljø}, proxyUrl: ${proxyUrl}`);
 
