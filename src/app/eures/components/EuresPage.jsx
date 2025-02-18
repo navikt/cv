6dce967ee8f88843c20471df2a59e095d595096e
BAH <bjorn.arne.hagen@nav.no>
2025-02-18 16:56:17 +0100

    Legger til dekoratør-breadcrumbs for Eures

--- src/app/eures/components/EuresPage.jsx
+++ src/app/eures/components/EuresPage.jsx
@@ -8,12 +8,24 @@ import EuresForhandsvisning from "@/app/eures/components/EuresForhandsvisning";
 import { useEures } from "@/app/_common/hooks/swr/useEures";
 import { useEffect, useState } from "react";
 import { euLand } from "@/app/_common/data/euLand";
+import { setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 
 export default function EuresPage() {
     const { eures } = useEures();
     const [kategorier, setKategorier] = useState([]);
     const [landSelectedOptions, setLandSelectedOptions] = useState([]);
     const [visHovedinnhold, setVisHovedinnhold] = useState(true);
+
+    useEffect(() => {
+        setBreadcrumbs([
+            { title: "Min side", url: "/minside" },
+            {
+                title: "EURES",
+                url: "/min-cv/eures",
+                handleInApp: true,
+            },
+        ]);
+    });
 
     useEffect(() => {
         if (eures) {
