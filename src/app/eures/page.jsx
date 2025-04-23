585b9811b65603d1b85366900f0f78c55c68761a
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-04-23 14:17:41 +0200

    Returnerer notfound på /eures i veilederløsningen

--- src/app/eures/page.jsx
+++ src/app/eures/page.jsx
@@ -1,5 +1,9 @@
 import EuresPage from "@/app/eures/components/EuresPage";
+import { serverConfig } from "@/app/_common/serverConfig";
+import NotFound from "@/app/not-found";
 
 export default async function Page() {
+    if (serverConfig.erVeileder) return <NotFound />;
+
     return <EuresPage />;
 }
