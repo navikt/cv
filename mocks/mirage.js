8fcfa9700854beeacc3ba10c4e7e1351a7145a4d
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2025-04-29 08:48:15 +0200

    Legger tilbake conditional imports, og kjører mirage på import

--- mocks/mirage.js
+++ mocks/mirage.js
@@ -7,22 +7,20 @@ import { arbeidsforholdMock } from "./mockData/arbeidsforholdMock";
 import { veilederDekoratørMock } from "./mockData/veilederDekoratørMock";
 import { amplitudeMock } from "./mockData/amplitudeMock";
 
-export function makeLocalhostServer() {
-    console.log("Kjører localhost mirage");
-    createServer({
-        namespace: "/min-cv",
-        logging: false,
-        routes() {
-            this.get("/api/samtykke/eures", samtykkeEuresMock);
-            this.get("/api/arbeidsforhold", arbeidsforholdMock);
-            this.get("/api/veileder/api/context/v2/aktivbruker", { aktivBruker: "04010100653" });
-            this.post("/api/veileder/api/context", { aktivBruker: "04010100653" });
-            this.get("/api/veileder/api/decorator", veilederDekoratørMock);
-            this.passthrough();
-            this.passthrough("https://dekoratoren.ekstern.dev.nav.no/*");
-            this.passthrough("https://www.nav.no/*");
-            this.passthrough("https://cdn.nav.no/*");
-            this.post("https://amplitude.nav.no/collect", amplitudeMock);
-        },
-    });
-}
+console.log("Kjører localhost mirage");
+createServer({
+    namespace: "/min-cv",
+    logging: false,
+    routes() {
+        this.get("/api/samtykke/eures", samtykkeEuresMock);
+        this.get("/api/arbeidsforhold", arbeidsforholdMock);
+        this.get("/api/veileder/api/context/v2/aktivbruker", { aktivBruker: "04010100653" });
+        this.post("/api/veileder/api/context", { aktivBruker: "04010100653" });
+        this.get("/api/veileder/api/decorator", veilederDekoratørMock);
+        this.passthrough();
+        this.passthrough("https://dekoratoren.ekstern.dev.nav.no/*");
+        this.passthrough("https://www.nav.no/*");
+        this.passthrough("https://cdn.nav.no/*");
+        this.post("https://amplitude.nav.no/collect", amplitudeMock);
+    },
+});
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,8 +6,6 @@ import { serverConfig } from "@/app/_common/serverConfig";
 import BorgerDekoratørWrapper from "@/app/_common/components/Dekoratør/BorgerDekoratørWrapper";
 import VeilederDekoratørWrapper from "@/app/_common/components/Dekoratør/VeilederDekoratørWrapper";
 import { logger } from "@navikt/next-logger";
-import { makeDemoServer } from "../../mocks/mirageDemo";
-import { makeLocalhostServer } from "../../mocks/mirage";
 
 export const dynamic = "force-dynamic";
 const sourceSansPro = Source_Sans_3({ subsets: ["latin"], adjustFontFallback: false });
@@ -16,11 +14,9 @@ async function RootLayout({ children }) {
     const { erVeileder, erDemoApp } = serverConfig;
 
     if (process.env.ER_DEMO_APP === "true") {
-        makeDemoServer();
-        logger.warn("Mirage mocks kjører i demo-modus!");
+        import("../../mocks/mirageDemo").then(() => logger.warn("Mirage mocks kjører i demo-modus!"));
     } else if (process.env.NODE_ENV === "development") {
-        makeLocalhostServer();
-        logger.warn("Mirage mocks kjører!");
+        import("../../mocks/mirage").then(() => logger.warn("Mirage mocks kjører!"));
     }
 
     logger.info(
