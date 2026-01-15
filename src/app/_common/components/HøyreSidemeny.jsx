ec5f0ee192dace4ae6b04ebbed21e57f12f24914
Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>
2026-01-15 09:16:31 +0100

    Feature/instruksjonsvideo (#201)
    * Legg til lenke til instruksjonsvideo i høyre sidemeny
    
    * Bump deploy-next-js-dev action
    
    * Åpne videolenke i ny fane

---
+++ src/app/_common/components/HøyreSidemeny.jsx
@@ -0,0 +1,25 @@
+import { Button, Link, VStack } from "@navikt/ds-react";
+import { ExternalLinkIcon, EyeIcon } from "@navikt/aksel-icons";
+import { LastNedCv } from "@/app/(minCV)/_components/lastNedCv/LastNedCv";
+
+export default function HøyreSidemeny({ cvLaster, setVisHovedinnhold }) {
+    return (
+        <VStack gap="4">
+            <Button
+                icon={<EyeIcon aria-hidden />}
+                variant="primary"
+                onClick={() => setVisHovedinnhold(false)}
+                disabled={cvLaster}
+            >
+                Forhåndsvis CV
+            </Button>
+            <LastNedCv />
+            <Link inlineText href="/min-cv/personvern">
+                Personvernserklæring for Min CV
+            </Link>
+            <Link inlineText rel="noopener noreferrer" href="https://vimeo.com/778413745" target="_blank">
+                Se video om hvordan du fyller ut CV-en din <ExternalLinkIcon title="(åpner i en ny fane)" />
+            </Link>
+        </VStack>
+    );
+}
